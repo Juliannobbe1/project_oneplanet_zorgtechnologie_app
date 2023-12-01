88e27870ab16f07277c06f66eca7e30740c3ff5f
Julian Nobbe <julian@juliannobbe.nl>
2023-12-01 12:14:00 +0100

    update loading test clientoverview

--- zorgtechnologieapp/test/client_overview_api_test.dart
+++ zorgtechnologieapp/test/client_overview_api_test.dart
@@ -38,3 +38,35 @@ void main() {
     expect(clientinterceptor.isDone, true);
   });
 }
+
+//   testWidgets('Load data in clientoverview screen',
+//       (WidgetTester tester) async {
+//     final productinteceptor = nock("http://192.168.72.182:5001")
+//         .get("/product/client/2e7cdd22-6d64-4e43-a8c6-9c6d3f3a7b3e")
+//       ..reply(
+//         200,
+//         jsonEncode([
+//           {
+//             "beschrijving":
+//                 "Een gehoorbeschermer die het gehoor beschermt tegen harde geluiden en gehoorverlies voorkomt. Het is lichtgewicht verstelbaar en comfortabel om te dragen.",
+//             "ID": "0b1b9708-9711-404e-944d-01b3659f15d5",
+//             "leverancierID": "45e22d68-9632-42b9-83ad-6ff5a4e577b0",
+//             "link": "https://example.com/gehoorbeschermer",
+//             "naam": "Gehoorbeschermer",
+//             "prijs": 19.99,
+//             "imageBase64": null
+//           },
+//         ]),
+//       );
+
+//     await tester.pumpWidget(const ProviderScope(
+//         child: PreferredOrientationWrapper(
+//       MaterialApp(
+//         home: Scaffold(
+//           body: clientSelectTile())))));
+
+//     // Use nock.cleanAll() to reset the interceptor and then check if there are no more pending mock requests
+//     nock.cleanAll();
+//     expect(productinteceptor.isDone, true);
+//   });
+// }
--- zorgtechnologieapp/test/client_overview_loading_test.dart
+++ zorgtechnologieapp/test/client_overview_loading_test.dart
@@ -1,15 +1,26 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:nock/nock.dart';
 import 'package:zorgtechnologieapp/main.dart';
 import 'package:zorgtechnologieapp/pages/home_page.dart';
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
-//! Ignore Data fetching log errors, this test is pure testing the loading time of one page to another
+//! Ignore Data fetching log Error fetching data: Request was: GET http://192.168.72.182:5001/product/newest, this test is pure testing the loading time of clientoverview
 
 void main() {
+  setUpAll(() {
+    nock.init();
+  });
+
+  setUp(() {
+    nock.cleanAll();
+  });
+
   testWidgets(
       'Clicking Keuzegids button navigates to ClientOverview performance test',
       (WidgetTester tester) async {
@@ -33,7 +44,37 @@ void main() {
 
     // Tap the Keuzegids button.
     final startTime = DateTime.now();
+
     await tester.tap(find.text('Keuzegids'));
+    final clientinterceptor = nock("http://192.168.72.182:5001")
+        .get("/client/wordtverzorgd/e040d519-dcc5-4969-86c3-54006f21656c")
+      ..reply(
+        200,
+        jsonEncode([
+          {
+            "probleem": "Veiligheid en toezicht",
+            "ID": "2e7cdd22-6d64-4e43-a8c6-9c6d3f3a7b3e"
+          },
+        ]),
+      );
+    final productinteceptor = nock("http://192.168.72.182:5001")
+        .get("/product/client/2e7cdd22-6d64-4e43-a8c6-9c6d3f3a7b3e")
+      ..reply(
+        200,
+        jsonEncode([
+          {
+            "beschrijving":
+                "Een gehoorbeschermer die het gehoor beschermt tegen harde geluiden en gehoorverlies voorkomt. Het is lichtgewicht verstelbaar en comfortabel om te dragen.",
+            "ID": "0b1b9708-9711-404e-944d-01b3659f15d5",
+            "leverancierID": "45e22d68-9632-42b9-83ad-6ff5a4e577b0",
+            "link": "https://example.com/gehoorbeschermer",
+            "naam": "Gehoorbeschermer",
+            "prijs": 19.99,
+            "imageBase64": null
+          },
+        ]),
+      );
+
     await tester.pumpAndSettle();
 
     // Use pumpAndSettle to ensure that the navigation has completed.
@@ -42,6 +83,10 @@ void main() {
 
     // Verify that we are on the Clienten Overzicht page.
     expect(find.text('Clienten Overzicht'), findsOneWidget);
+
+    //nock.cleanAll();
+    expect(clientinterceptor.isDone, true);
+    expect(productinteceptor.isDone, true);
 
     // Calculate the loading time
     final loadingTime = DateTime.now().difference(startTime).inMilliseconds;
